438f4d7407e0ef543592555edbeb17d3252057de
Priveasy Admin <Admin@Priveasy.org>
2020-12-26 23:34:39 -0500

    Updated admin page location

--- BadApple/BadApple/urls.py
+++ BadApple/BadApple/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [
-    path('dataTransferZone/', admin.site.urls),
+    path('AdminPortal/', admin.site.urls),
 	path('' , include('mainSite.urls')),
 ]
