0ee0e90ceedfc56bfa87d96e0141cf7e2e437b9a
Noah Troy <Admin@Priveasy.org>
2020-12-27 23:15:41 -0500

    Added a custom admin site

--- BadApple/mainSite/admin.py
+++ BadApple/mainSite/admin.py
@@ -1,10 +1,95 @@
 from django.contrib import admin
+from django.contrib.auth.models import User , Group
+from django.core.exceptions import PermissionDenied
+from django.urls import path
+from django.conf import settings
 from .models import *
 
-@admin.register(PRATemplate)
-class AccountContentsAdmin(admin.ModelAdmin):
-	list_display = ['public' , 'state' , 'entity' , 'technology' , 'approved']
+class CustomAdminSite(admin.AdminSite):
+	site_header = 'Bad Apple Database Management'
+	site_title = 'Bad Apple DB'
+	index_title = 'Database Contents'
 
-@admin.register(OversightCommission)
-class AccountContentsAdmin(admin.ModelAdmin):
-	list_display = ['public' , 'stateProvince' , 'cityTown' , 'approved']
+
+
+# Use custom, admin site:
+customAdminSite = CustomAdminSite(name = 'customAdminSite')
+
+customAdminSite.register(User)
+customAdminSite.register(Group)
+
+
+
+@admin.register(DatabaseManagerPermissions , site = customAdminSite)
+class DatabaseManagerPermissionsAdmin(admin.ModelAdmin):
+	list_display = ['user' , 'changesThisWeek' , 'changesLastWeek']
+
+
+
+@admin.register(PRATemplate , site = customAdminSite)
+class PRATemplateAdmin(admin.ModelAdmin):
+	list_display = ['subject' , 'state' , 'entity' , 'public' , 'approved']
+	exclude = ['createdOn' , 'updatedOn' , 'lastChangedBy' , 'daysUntilDeletion']
+
+
+	# Custom save behavior:
+	def add_view(self, request, form_url='', extra_context=None):
+		if (request.method == 'POST'):
+			if (request.POST.__contains__('lastChangedBy')):
+				request.POST = request.POST.copy()
+				request.POST.__setitem__('lastChangedBy' , request.user.username)
+				dbManagerObj = DatabaseManagerPermissions.objects.get(user = request.user)
+				if (dbManagerObj.changesThisWeek >= settings.WEEKLY_TOUCH_LIMIT):
+					raise PermissionDenied
+				dbManagerObj.changesThisWeek += 1
+				dbManagerObj.save()
+
+		return super().changeform_view(request, None, form_url, extra_context)
+
+	def change_view(self, request, object_id, form_url='', extra_context=None):
+		if (request.method == 'POST'):
+			if (request.POST.__contains__('lastChangedBy')):
+				request.POST = request.POST.copy()
+				request.POST.__setitem__('lastChangedBy' , request.user.username)
+				dbManagerObj = DatabaseManagerPermissions.objects.get(user = request.user)
+				if (dbManagerObj.changesThisWeek >= settings.WEEKLY_TOUCH_LIMIT):
+					raise PermissionDenied
+				dbManagerObj.changesThisWeek += 1
+				dbManagerObj.save()
+
+		return super().changeform_view(request, object_id, form_url, extra_context)
+
+
+
+@admin.register(OversightCommission , site = customAdminSite)
+class OversightCommissionAdmin(admin.ModelAdmin):
+	list_display = ['name' , 'stateProvince' , 'cityTown' , 'public' , 'approved']
+	exclude = ['createdOn' , 'updatedOn' , 'lastChangedBy' , 'daysUntilDeletion']
+
+
+	# Custom save behavior:
+	def add_view(self, request, form_url='', extra_context=None):
+		if (request.method == 'POST'):
+			if (request.POST.__contains__('lastChangedBy')):
+				request.POST = request.POST.copy()
+				request.POST.__setitem__('lastChangedBy' , request.user.username)
+				dbManagerObj = DatabaseManagerPermissions.objects.get(user = request.user)
+				if (dbManagerObj.changesThisWeek >= settings.WEEKLY_TOUCH_LIMIT):
+					raise PermissionDenied
+				dbManagerObj.changesThisWeek += 1
+				dbManagerObj.save()
+
+		return super().changeform_view(request, None, form_url, extra_context)
+
+	def change_view(self, request, object_id, form_url='', extra_context=None):
+		if (request.method == 'POST'):
+			if (request.POST.__contains__('lastChangedBy')):
+				request.POST = request.POST.copy()
+				request.POST.__setitem__('lastChangedBy' , request.user.username)
+				dbManagerObj = DatabaseManagerPermissions.objects.get(user = request.user)
+				if (dbManagerObj.changesThisWeek >= settings.WEEKLY_TOUCH_LIMIT):
+					raise PermissionDenied
+				dbManagerObj.changesThisWeek += 1
+				dbManagerObj.save()
+
+		return super().changeform_view(request, object_id, form_url, extra_context)
--- BadApple/mainSite/models.py
+++ BadApple/mainSite/models.py
@@ -1,10 +1,45 @@
 from django.db import models
+from django.conf import settings
+
+class DatabaseManagerPermissions(models.Model):
+	user = models.OneToOneField(settings.AUTH_USER_MODEL , on_delete = models.CASCADE)
+
+	# Limits:
+	changesThisWeek = models.IntegerField('Changes Made This Week (max = 10)' , default = 0)
+	changesLastWeek = models.IntegerField('Changes Made Last Week (max = 10)' , default = 0)
+
+
+
+	# Manage metadata:
+	class Meta:
+		verbose_name = 'Database Manager Permissions'
+		verbose_name_plural = 'Database Manager Permissions'
+
+
 
 class PRATemplate(models.Model):
+	# Choices:
+	ENTITIES = [
+				('PD' , 'Police Department'),
+				('SD' , 'Sheriff\'s Department')
+				]
+	SUBJECTS = [
+					('0' , 'Body Cameras'),
+					('1' , 'Drones'),
+					('2' , 'Facial Recognition'),
+					('3' , 'Thermographic Cameras'),
+					('4' , 'License Plate Readers'),
+					('5' , 'Memorandas of Understanding'),
+					('6' , 'Predictive Policing'),
+					('7' , 'ShotSpotter'),
+					('8' , 'Social Media Monitoring'),
+					('9' , 'Stingray')
+					]
+
 	# Filters:
 	state = models.CharField('State' , max_length = 30)
-	entity = models.CharField('Entity' , max_length = 50 , default = 'Police')
-	technology = models.CharField('Technology' , max_length = 50 , blank = True)
+	entity = models.CharField('Entity' , max_length = 2 , choices = ENTITIES , default = 'PD')
+	subject = models.CharField('Subject' , max_length = 4 , choices = SUBJECTS , default = '0')
 
 	# Template Contents:
 	letterBody = models.TextField('Letter Body' , max_length = 10000 , blank = True)
@@ -12,6 +47,9 @@ class PRATemplate(models.Model):
 	# Administrative:
 	createdOn = models.DateTimeField('Template Created On (Auto-Filled)' , auto_now_add = True)
 	updatedOn = models.DateTimeField('Template Last Updated On (Auto-Filled)' , auto_now = True)
+	lastChangedBy = models.CharField('Last Changed By' , max_length = 50 , blank = True)
+	delete = models.BooleanField('Delete' , default = False)
+	daysUntilDeletion = models.IntegerField('Days Until Deletion' , default = 30)
 
 	# Permissions:
 	approved = models.BooleanField('Template Approved' , default = False)
@@ -19,11 +57,18 @@ class PRATemplate(models.Model):
 
 
 
+	# Manage metadata:
+	class Meta:
+		verbose_name = 'PRA Template'
+		verbose_name_plural = 'PRA Templates'
+
+
+
 class OversightCommission(models.Model):
 	# Profile:
 	name = models.CharField('Name' , max_length = 60 , blank = True)
 	type = models.CharField('Type' , max_length = 60 , blank = True)
-	website = models.URLField('Website URL' , max_length = 250)
+	website = models.URLField('Website URL' , max_length = 250 , blank = True)
 
 	# Location:
 	country = models.CharField('Country' , max_length = 60 , blank = True , default = 'United States of America')
@@ -38,7 +83,17 @@ class OversightCommission(models.Model):
 	# Administrative:
 	createdOn = models.DateTimeField('Record Created On (Auto-Filled)' , auto_now_add = True)
 	updatedOn = models.DateTimeField('Record Last Updated On (Auto-Filled)' , auto_now = True)
+	lastChangedBy = models.CharField('Last Changed By' , max_length = 50 , blank = True)
+	delete = models.BooleanField('Delete' , default = False)
+	daysUntilDeletion = models.IntegerField('Days Until Deletion' , default = 30)
 
 	# Permissions:
 	approved = models.BooleanField('Record Approved' , default = False)
 	public = models.BooleanField('Record Public' , default = False)
+
+
+
+	# Manage metadata:
+	class Meta:
+		verbose_name = 'Oversight Commission'
+		verbose_name_plural = 'Oversight Commissions'
