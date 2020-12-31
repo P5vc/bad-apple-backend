3e755f9a6919348fbc8bd2793b5417e0970a15cf
Priveasy Admin <Admin@Priveasy.org>
2020-12-30 19:23:03 -0500

    Updated oversight commissions model

--- BadApple/mainSite/models.py
+++ BadApple/mainSite/models.py
@@ -41,7 +41,7 @@ class PRATemplate(models.Model):
 	subject = models.CharField('Subject' , max_length = 4 , choices = SUBJECTS , default = '0')
 
 	# Template Contents:
-	title = models.CharField('Title' , max_length = 250 , blank = True)
+	title = models.CharField('Title' , max_length = 300 , blank = True)
 	letterBody = models.TextField('Letter Body' , max_length = 10000 , blank = True)
 
 	# Administrative:
@@ -78,23 +78,39 @@ class OversightCommission(models.Model):
 	# Profile:
 	name = models.CharField('Name' , max_length = 60 , blank = True)
 	type = models.CharField('Type' , max_length = 4 , choices = TYPES , default = '0')
-	website = models.URLField('Website URL—https whenever possible)' , max_length = 250 , blank = True)
+	website = models.URLField('Website URL' , max_length = 300 , blank = True)
 
 	# Location:
 	country = models.CharField('Country' , max_length = 3 , choices = COUNTRIES , default = 'USA')
 	stateTerritoryProvince = models.CharField('State/Territory/Province' , max_length = 6 , choices = STATES_TERRITORIES_PROVINCES , default = 'USA-CA')
 	cityTown = models.CharField('City/Town' , max_length = 60 , blank = True)
+	postalCode = models.CharField('Postal Code' , max_length = 15 , blank = True)
 	address1 = models.CharField('Address (Line 1)' , max_length = 100 , blank = True)
 	address2 = models.CharField('Address (Line 2)' , max_length = 100 , blank = True)
-	postalCode = models.CharField('Postal Code' , max_length = 15 , blank = True)
 
-	# Contact Info:
+	# General Contact Info:
 	email = models.EmailField('Email' , blank = True)
-	phone = models.CharField('Phone Number—Format: +1 (123) 555-1234' , max_length = 21 , blank = True)
+	phone = models.CharField('Phone Number (Format: 1-123-555-1234)' , max_length = 18 , blank = True)
+	phoneTDD = models.CharField('TTD/TTY Phone Number (Format: 1-123-555-1234)' , max_length = 18 , blank = True)
+	fax = models.CharField('Fax Number (Format: 1-123-555-1234)' , max_length = 18 , blank = True)
+	contactForm = models.URLField('Contact Form URL' , max_length = 300 , blank = True)
+
+	# Press Contact Info:
+	pressEmail = models.EmailField('Press Email' , blank = True)
+	pressPhone = models.CharField('Press Phone Number (Format: 1-123-555-1234)' , max_length = 18 , blank = True)
+	pressContactForm = models.URLField('Press Contact Form URL' , max_length = 300 , blank = True)
+
+	# Contents:
+	aboutSummary = models.TextField('About/Summary' , max_length = 10000 , blank = True)
+	complaintInfo1 = models.URLField('Complaint Information (URL 1)' , max_length = 300 , blank = True)
+	complaintInfo2 = models.URLField('Complaint Information (URL 2)' , max_length = 300 , blank = True)
+	complaintForm = models.URLField('Complaint Form URL' , max_length = 300 , blank = True)
+	membersPage = models.URLField('Members Page URL' , max_length = 300 , blank = True)
+	faqPage = models.URLField('FAQ Page URL' , max_length = 300 , blank = True)
 
 	# Administrative:
-	createdOn = models.DateTimeField('Record Created On (Auto-Filled)' , auto_now_add = True)
-	updatedOn = models.DateTimeField('Record Last Updated On (Auto-Filled)' , auto_now = True)
+	createdOn = models.DateTimeField('Record Created On' , auto_now_add = True)
+	updatedOn = models.DateTimeField('Record Last Updated On' , auto_now = True)
 	lastChangedBy = models.CharField('Last Changed By' , max_length = 50 , blank = True)
 	delete = models.BooleanField('Delete' , default = False)
 	daysUntilDeletion = models.IntegerField('Days Until Deletion' , default = 30)
