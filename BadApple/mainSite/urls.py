721542e92e3b07bd7ba5ef08f38ee621ae0351de
Noah Troy <Admin@Priveasy.org>
2021-02-02 19:32:41 -0500

    Added initial Oversight Commission search page draft

--- BadApple/mainSite/urls.py
+++ BadApple/mainSite/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 				path('' , views.home , name = 'home'),
-				path('PRA/' , views.pra , name = 'pra')
+				path('PRA/' , views.pra , name = 'pra'),
+				path('Oversight/' , views.oversight , name = 'oversight')
 			]
