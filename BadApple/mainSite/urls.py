789357b88ec7a0cf48511afb2aac019c7b6754b6
Priveasy Admin <Admin@Priveasy.org>
2021-02-06 00:03:11 -0500

    Added commission overview page draft

--- BadApple/mainSite/urls.py
+++ BadApple/mainSite/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 				path('' , views.home , name = 'home'),
 				path('PRA/' , views.pra , name = 'pra'),
-				path('Oversight/' , views.oversight , name = 'oversight')
+				path('Oversight/' , views.oversight , name = 'oversight'),
+				path('Commission/<slug:slug>/' , views.commission , name = 'commission')
 			]
