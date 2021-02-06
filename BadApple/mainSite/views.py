2b6fcc6fabd177ce6108d980ee7bedea972a84ad
Noah Troy <Admin@Priveasy.org>
2021-02-06 00:03:11 -0500

    Added commission overview page draft

--- BadApple/mainSite/views.py
+++ BadApple/mainSite/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect , render
 from mainSite.models import PRATemplate , OversightCommission
 from mainSite.forms import PRATemplateForm , OversightCommissionForm
 from random import choice
+
+
+# Global Variables:
+backgroundImages = ['img/lucas-benjamin-wQLAGv4_OYs-unsplash.jpg' , 'img/steve-johnson-ctRJMubyj4o-unsplash.jpg' , 'img/joel-filipe-WjnF1Tp-p3I-unsplash.jpg' , 'img/rene-bohmer-YeUVDKZWSZ4-unsplash.jpg' , 'img/jr-korpa-SFT9G3pAxLY-unsplash.jpg' , 'img/rodion-kutsaev-pVoEPpLw818-unsplash.jpg' , 'img/vinicius-amnx-amano-OHPdgstNFGs-unsplash.jpg' , 'img/henrik-donnestad-V6Qd6zA85ck-unsplash.jpg' , 'img/pawel-czerwinski-l8DUam8vtbc-unsplash.jpg' , 'img/denise-chan-pXmbsF70ulM-unsplash.jpg']
 
 
 def home(request):
@@ -44,7 +48,6 @@ def oversight(request):
 			if (len(commissionObjects) > 0):
 				resultFound = True
 
-			backgroundImages = ['img/lucas-benjamin-wQLAGv4_OYs-unsplash.jpg' , 'img/steve-johnson-ctRJMubyj4o-unsplash.jpg' , 'img/joel-filipe-WjnF1Tp-p3I-unsplash.jpg' , 'img/rene-bohmer-YeUVDKZWSZ4-unsplash.jpg' , 'img/jr-korpa-SFT9G3pAxLY-unsplash.jpg' , 'img/rodion-kutsaev-pVoEPpLw818-unsplash.jpg' , 'img/vinicius-amnx-amano-OHPdgstNFGs-unsplash.jpg' , 'img/henrik-donnestad-V6Qd6zA85ck-unsplash.jpg' , 'img/pawel-czerwinski-l8DUam8vtbc-unsplash.jpg' , 'img/denise-chan-pXmbsF70ulM-unsplash.jpg']
 			commissions = []
 			for commissionObject in commissionObjects:
 				commission = {'image' : choice(backgroundImages) , 'commissionID' : '' , 'cityTown' : '' , 'commissionTitle' : '' , 'aboutText' : '' , 'websiteURL' : '.' , 'modificationDate' : ''}
@@ -65,3 +68,12 @@ def oversight(request):
 	else:
 		oversightForm = OversightCommissionForm()
 		return render(request , 'oversight.html' , {'oversightForm' : oversightForm , 'showResults' : False})
+
+
+def commission(request , slug):
+	try:
+		commissionObject = OversightCommission.objects.get(commissionID = str(slug) , approved = True , public = True)
+	except:
+		return redirect('oversight')
+
+	return render(request , 'commission.html' , {'image' : choice(backgroundImages) , 'name' : str(commissionObject.name) , 'type' : str(commissionObject.get_type_display()) , 'address1' : str(commissionObject.address1) , 'address2' : str(commissionObject.address2) , 'cityTown' : str(commissionObject.cityTown) , 'stateTerritoryProvince' : str(commissionObject.get_stateTerritoryProvince_display()) , 'email' : str(commissionObject.email) , 'phone' : str(commissionObject.phone) , 'ttdtty' : str(commissionObject.phoneTDD) , 'fax' : str(commissionObject.fax) , 'about' : str(commissionObject.aboutSummary) , 'website' : str(commissionObject.website) , 'contactForm' : str(commissionObject.contactForm) , 'pressForm' : str(commissionObject.pressContactForm) , 'complaintInfo1' : str(commissionObject.complaintInfo1) , 'complaintInfo2' : str(commissionObject.complaintInfo2) , 'complaintForm' : str(commissionObject.complaintForm) , 'members' : str(commissionObject.membersPage) , 'faq' : str(commissionObject.faqPage) , 'updated' : str(commissionObject.updatedOn.strftime('%B %d, %Y'))})
