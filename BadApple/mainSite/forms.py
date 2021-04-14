bc75f1724b044288a67921d9d00d0b4329647017
Noah Troy <Admin@Priveasy.org>
2021-04-14 00:12:39 -0400

    Added Bad Apple Database search form

--- BadApple/mainSite/forms.py
+++ BadApple/mainSite/forms.py
@@ -1,6 +1,7 @@
 from django.utils.translation import gettext_lazy as _
-from django.forms import ModelForm , Select , TextInput , CharField , Textarea
+from django.forms import Form , ModelForm , Select , TextInput , CharField , IntegerField , ChoiceField , Textarea
 from mainSite.models import PRATemplate , OversightCommission , Tip
+import mainSite.extendedModels.modelCodes as choices
 
 
 
@@ -36,3 +37,13 @@ class TipFormCAPTCHA(ModelForm):
 		model = Tip
 		fields = ['topic' , 'message']
 		widgets = {'topic' : Select(attrs = {'class' : 'form-control'}) , 'message' : Textarea(attrs = {'class' : 'form-control' , 'rows' : '8' , 'placeholder' : _('Enter your message here...')})}
+
+
+
+class BadAppleForm(Form):
+	firstName = CharField(max_length = 150 , required = False , widget = TextInput(attrs = {'class' : 'form-control' , 'placeholder' : _('First Name')}))
+	lastName = CharField(max_length = 150 , required = False , widget = TextInput(attrs = {'class' : 'form-control' , 'placeholder' : _('Last Name')}))
+	cityTownCounty = CharField(max_length = 150 , required = False , widget = TextInput(attrs = {'class' : 'form-control' , 'placeholder' : _('City, Town, or County')}))
+	stateTerritoryProvince = ChoiceField(choices = ([('' , _('Any'))] + choices.STATES_TERRITORIES_PROVINCES) , required = False , widget = Select(attrs = {'class' : 'form-control'}))
+	year = IntegerField(max_value = 3000 , min_value = 1000 , required = False , widget = TextInput(attrs = {'class' : 'form-control' , 'placeholder' : _('Year of Incident')}))
+	policyCategory = ChoiceField(choices = ([('' , _('Any'))] + choices.POLICY_CATEGORIES) , required = False , widget = Select(attrs = {'class' : 'form-control'}))
