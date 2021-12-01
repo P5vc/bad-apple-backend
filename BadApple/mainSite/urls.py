086d2caa8c1368dfd14bc7a4e3a8cc0a7dd6d9cf
Steve Phillips <steve@tryingtobeawesome.com>
2021-11-30 20:28:06 -0800

    mainSite.urls: Whitespace

--- BadApple/mainSite/urls.py
+++ BadApple/mainSite/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from mainSite import views
 
 urlpatterns = [
-				path('' , views.home , name = 'home'),
-				path('LearnMore/' , views.documentation , name = 'documentation'),
-				path('PRA/' , views.pra , name = 'pra'),
-				path('Oversight/' , views.oversight , name = 'oversight'),
-				path('Commission/<slug:slug>/' , views.commission , name = 'commission'),
-				path('Tip/' , views.tip , name = 'tip'),
-				path('BadAppleDatabase/' , views.badApple , name = 'badApple'),
-				path('Officer/<slug:slug>/' , views.officer , name = 'officer'),
-				path('Report/<slug:slug>/' , views.report , name = 'report'),
-				path('API/' , views.apiDocumentation , name = 'apiDocumentation'),
-				path('API/<slug:slug>/' , views.apiQuery , name = 'apiQuery')
-			]
+	path('' , views.home , name = 'home'),
+	path('LearnMore/' , views.documentation , name = 'documentation'),
+	path('PRA/' , views.pra , name = 'pra'),
+	path('Oversight/' , views.oversight , name = 'oversight'),
+	path('Commission/<slug:slug>/' , views.commission , name = 'commission'),
+	path('Tip/' , views.tip , name = 'tip'),
+	path('BadAppleDatabase/' , views.badApple , name = 'badApple'),
+	path('Officer/<slug:slug>/' , views.officer , name = 'officer'),
+	path('Report/<slug:slug>/' , views.report , name = 'report'),
+	path('API/' , views.apiDocumentation , name = 'apiDocumentation'),
+	path('API/<slug:slug>/' , views.apiQuery , name = 'apiQuery'),
+]
