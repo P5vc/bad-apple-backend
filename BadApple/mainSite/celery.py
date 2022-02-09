561c256957f580727b97e700d4ae1a03f61a1e99
Priveasy Admin <Admin@Priveasy.org>
2022-02-09 01:02:40 -0500

    Corrected celery configuration

--- BadApple/mainSite/celery.py
+++ BadApple/mainSite/celery.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from celery import Celery
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE' , 'BadApple.settings.pro')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE' , 'BadApple.settings')
 
 app = Celery('mainSite')
 app.config_from_object('django.conf:settings' , namespace = 'CELERY')
